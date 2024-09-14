08ee0d82a013d3468150a39c46e21d4b5f1d2e4d
San Nguyen <vinhsannguyen91@gmail.com>
2024-09-14 09:17:53 +0900

    chore: use static profile for firefox
    Signed-off-by: San Nguyen <vinhsannguyen91@gmail.com>

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  home.file.".mozilla/firefox/n4c30aj4.default-release/user.js".source = ./user.js;
-  home.file.".mozilla/firefox/n4c30aj4.default-release/chrome".source = ./chrome;
-  home.file.".mozilla/firefox/n4c30aj4.default-release/chrome".recursive = true;
+  home.file.".mozilla/firefox/sand/user.js".source = ./user.js;
+  home.file.".mozilla/firefox/sand/chrome".source = ./chrome;
+  home.file.".mozilla/firefox/sand/chrome".recursive = true;
 }
